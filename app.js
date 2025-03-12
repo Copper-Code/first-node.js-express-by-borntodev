2d356be1c72964e1ae94f0d6bd43f1ea04265002
Copper Code <123915577+Copper-Code@users.noreply.github.com>
2025-03-12 18:05:02 +0700

    Add al files of Project
    Hello Copper😍

---
+++ app.js
@@ -0,0 +1,21 @@
+
+import chalk from 'chalk';
+import express from 'express';
+import debug from 'debug';
+import morgan from 'morgan';
+//const express = require('express');
+//const chalk = require('chalk')
+//const debug = require('debug')('app');
+const app = express();
+const port = 3000;// ห้ามต้องพอร์ตทับกับพอร์ตอื่นๆ ในเครื่องเรา
+
+app.use(morgan('combined'))
+app.get("/",(req,res)=>{ //ถ้าเขาเข้ามาให้ส่งค่าอะไรกลับ
+    res.send("Copper Tech KINNAPHAT")
+
+})
+
+app.listen(port,()=>{
+
+    debug("Listening on port "+chalk.green(port));
+})
